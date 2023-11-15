0b49e6432939cfba71ae5ec94d7bcce0d55253e2
rtef119 <rtef119@gmail.com>
2023-11-15 16:24:35 +0100

    itstep10

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import sqlite3
 
 connection = sqlite3.connect("itstep10.sl3")
 cur = connection.cursor()
-
-print(connection)
-print(cur)
+#cur.execute("CREATE TABLE first_table(name TEXT);")
+cur.execute("INSERT INTO first_table (name) VALUES ('Kira');")
+cur.execute("INSERT INTO first_table (name) VALUES ('Arseniy');")
+cur.execute("INSERT INTO first_table (name) VALUES ('Dmutro');")
+cur.execute("INSERT INTO first_table (name) VALUES ('Artem');")
+cur.execute("INSERT INTO first_table (name) VALUES ('Evgen');")
+cur.execute("INSERT INTO first_table (name) VALUES ('Svatik');")
+cur.execute("INSERT INTO first_table (name) VALUES ('Olekcandr');")
+connection.commit()
 connection.close()
